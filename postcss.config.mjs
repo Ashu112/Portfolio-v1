a44e916c47685140e68db9d554e6800648eef520
Ashu112 <kashu2611@gmail.com>
2025-10-17 07:20:33 +0530

    tailwind fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
-// postcss.config.mjs
-export default {
+const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
+export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex flex-row gap-[30px] border border-red-500 rounded-md items-center">
-      <div className="text-[24px] font-light bg-black">Ashu</div>
+      <div className="text-sm font-light">Ashu</div>
       <div className="text-sm font-bold">Home</div>
     </div>
 
