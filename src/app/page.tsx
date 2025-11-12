ebfe025b793f008ad28fe0c27b06940737a4804f
Ashu112 <kashu2611@gmail.com>
2025-11-12 16:12:03 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 
-
 export default function Home() {
   return (
-    <div className="flex flex-row gap-[30px] border border-red-500 rounded-md items-center">
+    <div className="flex flex-row gap-7 rounded-md items-center">
       <div className="text-sm font-light">Ashu</div>
       <div className="text-sm font-bold">Home</div>
     </div>
