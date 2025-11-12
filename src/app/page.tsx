a41d9d9d5216a7fc08759235fe7fa661c8f36a91
Ashu112 <kashu2611@gmail.com>
2025-11-13 01:55:23 +0530

    prettier formatter added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-
 export default function Home() {
   return (
     <div className="flex flex-row gap-7 rounded-md items-center">
       <div className="text-sm font-light">Ashu</div>
       <div className="text-sm font-bold">Home</div>
     </div>
-
   );
 }
