769fb8fc02164684d9e6c0a021f5f9ff1a95e0f7
Ashu112 <kashu2611@gmail.com>
2025-11-15 20:06:33 +0530

    pre commit hook added

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,27 +2,28 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <div className="flex justify-between gap-10 px-16 py-20">
-      <div className="flex flex-col gap-8 px-20">
-        <h1 className="text-green-400 text-7xl font-bold">Ashutosh Kumar</h1>
-        <p className="text-xl text-white max-w-3xl w-full mx-auto">
-          I'm a software developer from IIT Guwahati with 3+ years of experience
+    <div className="flex items-center justify-between px-16 py-24 gap-12">
+      <div className="flex flex-col gap-6 max-w-3xl">
+        <h1 className="text-green-400 text-6xl font-bold">Ashutosh Kumar</h1>
+        <h2 className="text-3xl text-gray-300 font-semibold">
+          Software Development Engineer
+        </h2>
+        <p className="text-xl text-white  leading-8">
+          I'm a Software developer from IIT Guwahati with 3+ years of experience
           creating fast, scalable, and user-focused web applications. I've
           worked across e-commerce, security, and AI-driven platforms using
           React, Next.js, Astro, and GraphQL.
         </p>
       </div>
 
-    <div className="w-[350px] h-[300px] relative">
-     <Image
-        src="/images/portfolio.jpg"
-        alt="Hero Image"
-        className="rounded-4xl object-cover shadow-2xl"
-        width={400}
-        height={500}
-      />
-    </div>
-     
+      <div className="relative w-[350px] h-[450px]">
+        <Image
+          src="/images/portfolio.jpg"
+          alt="Profile"
+          fill
+          className="rounded-3xl object-cover shadow-xl"
+        />
+      </div>
     </div>
   );
 }
