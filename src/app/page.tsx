ddac70e3b8262599fa0accf9e6ce417e19560083
Ashu112 <kashu2611@gmail.com>
2025-11-15 14:37:28 +0530

    hero image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Hero from "@/components/hero/Hero";
 export default function Home() {
   return (
     <div className="bg-global min-h-screen mt-12">
-     <Header/>
-     <Hero/>
+      <Header />
+      <Hero />
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import { Mail, Sun } from "lucide-react";
 
 function Header() {
   return (
     <div className="flex justify-between items-center px-10">
-        <Link href="/">
-             <Image src="/images/logo_black.png" alt="Logo" width={100} height={100} className="rounded-md" />
-        </Link>
-       
+      <Link href="/">
+        <Image
+          src="/images/logo_black.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="rounded-md"
+        />
+      </Link>
+
       <div className="flex gap-8 text-white text-3xl font-medium font-sans">
         <Link href="/">About</Link>
-          <Link href="/">Projects</Link>
-         <Link href="/">Resume</Link>
-          <Link href="/">Blogs</Link>
+        <Link href="/">Projects</Link>
+        <Link href="/">Resume</Link>
+        <Link href="/">Blogs</Link>
       </div>
-      <Link href="/" className="text-yellow-200 font-medium text-3xl font-sans">Contact Me</Link>
+      <div className="flex gap-5  items-center text-green-400">
+        <Link href="/">
+          <Mail className="size-7 mt-2" />
+        </Link>
+        <button className="hover:text-white cursor-pointer">
+          <Sun className="size-7 mt-2" />
+        </button>
+      </div>
     </div>
   );
 }
